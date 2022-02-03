7f16c6acd7a0883eee638315c0ce9852b8e8956d
Predators <31931530+predators46@users.noreply.github.com>
2022-02-03 08:43:58 +0800

    Update my_timer.h

--- include/my_timer.h
+++ include/my_timer.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16,21 +16,28 @@
 #ifndef MY_TIMER_H
 #define MY_TIMER_H
 
-#include "my_global.h"    /* C_MODE_START, C_MODE_END */
-#include "mysql/psi/psi.h" /* PSI_thread_key, PSI_mutex_key, PSI_memory_key */
+/**
+  @file include/my_timer.h
+*/
+
+#include "my_config.h"
+
+struct st_my_timer;
 
 /* POSIX timers API. */
 #ifdef HAVE_POSIX_TIMERS
 # include <time.h>  /* timer_t */
+
   typedef timer_t   os_timer_t;
-#elif HAVE_KQUEUE_TIMERS
+#elif defined(HAVE_KQUEUE_TIMERS)
 # include <sys/types.h> /* uintptr_t */
+
   typedef uintptr_t os_timer_t;
-#elif _WIN32
+#elif defined(_WIN32)
   typedef struct st_os_timer
   {
     HANDLE timer_handle;
-    my_bool timer_state;
+    bool timer_state;
   } os_timer_t;
 #endif
 
@@ -46,17 +53,11 @@ struct st_my_timer
   void (*notify_function)(my_timer_t *);
 };
 
-C_MODE_START
-
-#ifdef HAVE_PSI_INTERFACE
-extern PSI_thread_key key_thread_timer_notifier;
-#endif
-
 /* Initialize internal components. */
-int my_timer_initialize(void);
+int my_timer_initialize();
 
 /* Release any resources acquired. */
-void my_timer_deinitialize(void);
+void my_timer_deinitialize();
 
 /* Create a timer object. */
 int my_timer_create(my_timer_t *timer);
@@ -69,7 +70,5 @@ int my_timer_cancel(my_timer_t *timer, int *state);
 
 /* Delete a timer object. */
 void my_timer_delete(my_timer_t *timer);
-
-C_MODE_END
 
 #endif /* MY_TIMER_H */
